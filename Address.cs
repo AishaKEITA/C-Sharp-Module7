19a7ea90ce254eaa222b3022370eded0a8492856
aisha <aishakeita6@gmail.com>
2022-12-28 17:11:56 +0000

    Added getter and setters for the Address class

---
+++ Address.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmment7
+{
+    internal class ContactInfo
+    {
+        //fields
+        private string city;
+        private string postCode;
+        private ContactInfo contact;
+        private Gender gender;
+        /// <summary>
+        /// constructor contact info
+        /// </summary>
+        /// <param name="city"></param>
+        /// <param name="postCode"></param>
+        public ContactInfo(string city, string postCode, Gender gender)
+        {
+            this.city = city;
+            this.postCode = postCode;
+            this.gender = gender;
+        }
+         /// <summary>
+        /// contact constructor
+        /// </summary>
+        public ContactInfo()
+        {
+        }
+        /// <summary>
+        /// getter and setter for city
+        /// </summary>
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        /// <summary>
+        /// getter and setter for post code
+        /// </summary>
+        public string PostCode
+        {
+            get { return postCode; }
+            set { postCode = value; }
+        }
+
+        /// <summary>
+        /// address constructor with a parameter
+        /// </summary>
+        /// <param name="address"></param>
+        public ContactInfo(ContactInfo address)
+        {
+            this.contact = address;
+        }
+
+        /// <summary>
+        /// method to check so city is not empty or null string
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            if (string.IsNullOrEmpty(city))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
