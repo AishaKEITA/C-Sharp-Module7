eeedd9f4093970d498626136e92e523db60bd31b
aisha <aishakeita6@gmail.com>
2022-12-28 18:01:02 +0000

    Added fields for guest choices

---
+++ GuestChoices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmment7
+{
+    internal class GuestChoices
+    {
+        private int numberOfRooms;
+        public int floors;
+        private int checkInDate;
+        private int checkOutDate;
+        private RoomTypes roomType;
+
+    }
+}
