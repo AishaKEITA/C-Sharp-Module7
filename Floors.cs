0d6b6b9e969596bfceb7e84b9d30f355eacd7978
aisha <aishakeita6@gmail.com>
2022-12-30 14:31:00 +0000

    Added enum for floors

---
+++ Floors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmment7
+{
+    public enum Floors
+    {
+        Floor1 = 1,
+        Floor2 = 2,
+        Floor3 = 3,
+        Floor4 = 4,
+        Floor5 = 5,
+        Floor6 = 6
+    }
+}
