7bd6b854068115d3682736a482bf57fa4a9584a5
aisha <aishakeita6@gmail.com>
2023-01-03 10:18:15 +0000

    Added an emum class for number of children

---
+++ NumberOfChildren.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmment7
+{
+    public enum NumberOfChildren
+    {
+        child1 = 0,
+        child2 = 1,
+        child3 = 2,
+        child4 = 3,
+        child5 = 4,
+        child6 = 5,
+        child7 = 6,
+        child8 = 7,
+    }
+}
