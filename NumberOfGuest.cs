58b3962cba88b1f6bfbb429324274963207fd65e
aisha <aishakeita6@gmail.com>
2023-01-03 10:15:14 +0000

    Added number of guest enum class

---
+++ NumberOfGuest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmment7
+{
+    public enum NumberOfGuest
+    {
+        Adult1 = 1,
+        Adult2 = 2,
+        Adult3 = 3,
+        Adult4 = 4,
+        Adult5 = 5,
+        Adult6 = 6,
+        Adult7 = 7,
+        Adult8 = 8,
+     }
+}
